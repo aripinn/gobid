805e97bedf7d3705772bdf330245c82658eb7dfe
ARP <111234926+aripinn@users.noreply.github.com>
2023-02-16 09:17:51 +0700

    Add login auth & admin dashboard route

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\DashboardController;
 use Illuminate\Routing\Router;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\LoginController;
@@ -21,6 +22,9 @@ Route::get('/', function () {
 });
 
 Route::get('/login', [LoginController::class, 'index']);
+Route::post('/login', [LoginController::class, 'authenticate']);
 
 Route::get('/register', [RegisterController::class, 'index']);
 Route::post('/register', [RegisterController::class, 'create']);
+
+Route::get('/dashboard', [DashboardController::class, 'index']);
