a0e6c4f8f6eb871814e2703a9bbe3c77e1d9c114
ARP <111234926+aripinn@users.noreply.github.com>
2023-02-16 11:57:55 +0700

    Add Authorization comment

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -7,6 +7,17 @@ use Illuminate\Http\Request;
 class DashboardController extends Controller
 {
     public function index(){
+        // Manual Authorization
+        // if(auth()->guest()){
+        //     return view('pages.login');
+        // }
+        // if(auth()->user()->role === 'member'){
+        //     abort(403);
+        // }
+
+        // Using gates authorization
+        // $this->authorize('admin');
+
         return view('pages.admin.index');
     }
 }
