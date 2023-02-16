b1f07a6dc5fadfe88e9f85a3faf3fd5121a65b59
ARP <111234926+aripinn@users.noreply.github.com>
2023-02-16 09:18:35 +0700

    make login work

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -3,10 +3,25 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class LoginController extends Controller
 {
     public function index(){
         return view('pages.login', []);
+    }
+
+    public function authenticate(Request $request){
+        $credentials = $request->validate([
+            'username' => 'required|max:255',
+            'password' => 'required|max:255'
+        ]);
+
+        if (Auth::attempt($credentials)){
+            $request->session()->regenerate();
+            return redirect()->intended('/dashboard');
+        }
+
+        return back()->with('loginFailed', 'Login failed!');
     }
 }
--- resources/views/pages/login.blade.php
+++ resources/views/pages/login.blade.php
@@ -20,27 +20,44 @@
           <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
         </div>
         @endif
-        
+
+        {{--Login failed alert --}}
+        @if (session()->has('loginFailed'))
+        <div class="alert alert-danger alert-dismissible fade show" role="alert">
+          {{ session('loginFailed') }}
+          <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+        </div>
+        @endif
+
         <div class="card border-light shadow px-5 py-4">
           <h1 class="mb-4 fw-bold">Login</h1>
-          <form>
+          <form action="/login" method="post">
+            @csrf
             <!-- Username input -->
             <div class="form-floating mb-4">
-              <input type="text" id="username" class="form-control"
-                placeholder="Enter username" required/>
+              <input type="text" name="username" id="username" class="form-control @error('username') is-invalid @enderror" value="{{ old('username') }}" placeholder="Enter username" autofocus/>
               <label class="form-label" for="username">Username</label>
+              @error('username')
+              <div class="invalid-feeddback text-danger">
+                {{ $message }}
+              </div>
+              @enderror
             </div>
   
             <!-- Password input -->
             <div class="form-floating mb-3">
-              <input type="password" id="password" class="form-control"
-                placeholder="Enter password" required/>
+              <input type="password" name="password" id="password" class="form-control @error('password') is-invalid @enderror" placeholder="Enter password"/>
               <label class="form-label" for="password">Password</label>
+              @error('password')
+              <div class="invalid-feeddback text-danger">
+                {{ $message }}
+              </div>
+              @enderror
             </div>
   
             {{-- Buttons --}}
             <div class="text-center text-lg-start pt-2">
-              <button type="button" class="btn btn-primary btn-lg d-flex justify-content-center align-items-center gap-1 fw-semibold" style="padding-left: 2rem; padding-right: 2rem;">
+              <button type="submit" class="btn btn-primary btn-lg d-flex justify-content-center align-items-center gap-1 fw-semibold" style="padding-left: 2rem; padding-right: 2rem;">
                 <span>Login</span>
                 <iconify-icon inline icon="material-symbols:login"></iconify-icon>
               </button>
