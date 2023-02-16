316c6abf6b993ff38f542f0ea738dab24dc527e2
ARP <111234926+aripinn@users.noreply.github.com>
2023-02-16 11:56:13 +0700

    Update admin dashboard view placeholder

--- resources/views/pages/admin/index.blade.php
+++ resources/views/pages/admin/index.blade.php
@@ -1,1 +1,11 @@
 This is admin dashboard
+
+<p>This paragraph is visible to everyone that can access this page</p>
+
+@can('admin')
+    <p>This paragraph is only show to admin</p>
+@endcan
+
+@can('staff')
+    <p>This paragraph is only show to staff</p>
+@endcan
