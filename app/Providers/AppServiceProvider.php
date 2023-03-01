9241d868fe8a63c633118ed833ae820c66fedd7f
ARP <111234926+aripinn@users.noreply.github.com>
2023-03-01 08:30:42 +0700

    Define gates, pagination & rupiah number format

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,10 @@
 
 namespace App\Providers;
 
+use App\Models\User;
+use Illuminate\Pagination\Paginator;
+use Illuminate\Support\Facades\Gate;
+use Illuminate\Support\Facades\Blade;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -23,6 +27,23 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        Gate::define('Admin', function(User $user){
+            return $user->role == 'Admin';
+        });
+
+        Gate::define('Staff', function(User $user){
+            return $user->role == 'Staff';
+        });
+
+        Gate::define('Member', function(User $user){
+            return $user->role == 'Member';
+        });
+
+        // Pagination
+        Paginator::useBootstrapFive();
+
+        Blade::directive('rupiah', function ($amount) {
+            return "<?php echo 'Rp' . number_format($amount,0,',','.'); ?>";
+        });
     }
 }
