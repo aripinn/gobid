b4d7309d4a2f617bd335dadfc2d50972e98a67c9
ARP <111234926+aripinn@users.noreply.github.com>
2023-03-03 13:24:05 +0700

    Show only open auction in home

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -8,7 +8,7 @@ use Illuminate\Http\Request;
 class HomeController extends Controller
 {
     public function index(){
-        $auctions = Auction::latest()->limit(4)->get();
+        $auctions = Auction::latest()->limit(4)->where('status', 'open')->get();
         
         return view('pages.home', [
             'auctions' => $auctions,
