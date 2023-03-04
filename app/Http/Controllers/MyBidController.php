e47d799e49479d84fbb5e9c97fd05006737255b5
ARP <111234926+aripinn@users.noreply.github.com>
2023-03-04 23:02:34 +0700

    Update mybid only show highest bid foreach auction

--- app/Http/Controllers/MyBidController.php
+++ app/Http/Controllers/MyBidController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Http\Request;
 use App\Models\Bid;
@@ -12,10 +13,20 @@ class MyBidController extends Controller
         if(!isset(Auth::user()->id)){
             return redirect('/login');
         }
+
+        // Get highest bid foreach auction
+        $bids = Bid::where('user_id', Auth::user()->id)
+                    ->whereIn('id', function ($query) {
+                        $query->select(DB::raw('MAX(id)'))
+                            ->from('bids')
+                            ->whereRaw('bids.user_id = ?', [Auth::user()->id])
+                            ->groupBy('auction_id');
+                    })
+                    ->latest()->get();
         
         return view('pages.mybid', [
             'title' => 'My Bid',
-            'bids' => Bid::where('user_id',Auth::user()->id)->get(),
+            'bids' => $bids,
         ]);
     }
 }
