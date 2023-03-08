24251ebea68eaf76fc9806bda29c42167a35e388
ARP <111234926+aripinn@users.noreply.github.com>
2023-03-08 11:16:39 +0700

    Add app icon

--- resources/views/admin/layouts/app.blade.php
+++ resources/views/admin/layouts/app.blade.php
@@ -11,6 +11,9 @@
   <meta content="" name="description">
   <meta content="" name="keywords">
 
+  {{-- App Icon --}}
+  <link rel="shortcut icon" href="{{ asset('assets/img/GB.ico') }}">
+  
   <!-- Favicons -->
   <link href="/assets/img/favicon.png" rel="icon">
   <link href="/assets/img/apple-touch-icon.png" rel="apple-touch-icon">
